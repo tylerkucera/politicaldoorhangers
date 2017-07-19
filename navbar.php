9ab5b8c8753956139572f0aff0148a3e2159b680
Tyler Kucera <tkucera10@gmail.com>
2017-07-19 07:58:14 +0000

    Delete old files and start new site

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<ul class="navbar dark-blue-bg">
+	<li>
+		<a href="testimonials.php">Testimonials</a>
+	</li>
+	<li>
+		<a href="contact.php">Contact</a>
+	</li>
+	<li>
+		<a href="about.php">About Us</a>
+	</li>
+	<li>
+		<a href="index.php">Home</a>
+	</li>
+</ul>
+
+<?php echo(basename(__FILE__, '.php')); ?>
---
+++ testimonials.php
@@ -0,0 +1,13 @@
+ <!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Political Doorhangers - Testimonials</title>
+        <script src="scripts/jquery-3.2.1.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="css/main.css?1"/>
+    </head>
+
+    <body class="cream-bg">
+        <?php include_once("navbar.php");?>
+
+    </body>
+</html>

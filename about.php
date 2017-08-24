506ab0b00b3987dfaa8044dfbc7f4fac38b4bd8d
Tyler Kucera <tkucera10@gmail.com>
2017-08-23 22:27:28 -0400

    Add text to about page

--- about.php
+++ about.php
@@ -10,5 +10,18 @@
     <body>
         <?php include_once("navbar.php");?>
 
+        <div>
+        	<p>
+	        	The recent U.S. Supreme Court ruling that now allows corporations and unions to advertise directly in political contests is a game changer in terms of how campaigns of the future will be run. The level of campaigning sophistication required for a candidate to ultimately emerge victorious has increased exponentially. We have developed a proven strategy that will effectively deliver your candidates’ message to potential voters, and will aid in increasing name recognition.
+	        </p>
+	        <p>
+	        	Building on the lessons we learned from our statewide involvement in the Ohio Issue 3 Campaign in 2009 (the successful casino gambling referendum), we can now offer a superior method of reaching targeted populations at a very attractive cost per household.
+	        </p>
+	        <p>
+	        	Our model is simple: When political messages are sent via the U.S. Postal Service, the mailer is mixed in with other mail and often is discarded without so much as a glance. However, with door hangers, the resident has to handle the message, and is therefore far more prone to look at it. Additionally, we can deliver door hangers to more tightly defined demographic areas for approximately one-third less than the cost of a mailer.
+	        </p>
+        </div>
+        
+
     </body>
 </html> 
